bbdaa950b52c42546dce6b4e5d12e19813adf54d
Roni PC <roninds14@gmail.com>
2020-10-24 18:19:02 -0300

    Correção do NameSpace

--- PHP/Config/Config.php
+++ PHP/Config/Config.php
@@ -5,7 +5,7 @@ $newName = "operacao.txt";
 spl_autoload_register( function($nameClass) {
 
 	$dirClass = $_SERVER["DOCUMENT_ROOT"] . DIRECTORY_SEPARATOR . "Analisador_Lexico_Calc" . DIRECTORY_SEPARATOR ."PHP". DIRECTORY_SEPARATOR ."Class";
-	$filename = $dirClass . DIRECTORY_SEPARATOR . $nameClass . ".php";
+	$filename = str_replace ("\\", "/", $dirClass . DIRECTORY_SEPARATOR . $nameClass . ".php");
 
 	if(file_exists($filename))
 		require_once( $filename );
